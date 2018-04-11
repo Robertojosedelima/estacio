f56210f5ecdce7086290bbb55f9d99f6ae6b6dc5
drop- <drop-@DESKTOP-O9DSETS>
2018-04-11 17:26:18 -0300

    em andamento

--- br.com.autosistem.visao/visao.painel.administrador/GerenciaPainelAdministrador.php
+++ br.com.autosistem.visao/visao.painel.administrador/GerenciaPainelAdministrador.php
@@ -1,26 +1,125 @@
 <?php
-echo"Painel administrador "
-. ""
-        . ""
-        . ""
-        . ""
-        . "aqui vai ter uma tabela com ultimo cadastro adm de todas as classes"
-        . ""
-        . "vai ter uma tela com aniversarios do més "
-        . ""
-        . ""
-        . "no financeiro valores das vendas de peças e mão de obra "
-        . "ultimas compras a fornecedores "
-        . "ultimo produto cadastrado no estoque"
-        . "ultimo usuario do sistema"
-        . "mensagem de estoque baixo "
-        . "salario mais alto "
-        . "";
 
+?>
+<fieldset>
+ <legend>DADOS DO USUARIO</legend>
+   <br/> 
+ <table border="1" bgcolor="#9cf">
+     <th>Nome</th>
+     <th>Função</th>
+     <th>Usuario</th>
+     <th>Email</th>
+     <th>Telefone</th>
+     
+     <tr>
+     <td>USUARIO</td>
+     <td>CLIENTES</td>
+     <td>FORNECEDORES</td>
+     <td>FUNÇÕES</td>
+     <td>FUNCIONARIOS</td>
+     </tr>
 
-/* echo
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ </table>
+</fieldset>
+<fieldset>
+ <legend>DADOS DA EMPRESA</legend>
+   <br/> 
+ <table border="1" bgcolor="#9cf">
+     <th>Nome da Empresa</th>
+     <th>Razão Social</th>
+     <th>Cnpj</th>
+     <th>Endereço</th>
+     <th>Telefone</th>
+     
+     <tr>
+     <td>NOME</td>
+     <td>RAZAO SOCIAL</td>
+     <td>CNPJ</td>
+     <td>ENDEREÇO</td>
+     <td>TELEFONE</td>
+     </tr>
 
+ </table>
+</fieldset>
+<fieldset>
+ <legend>DADOS ADMINISTRATIVOS</legend>
+   <br/> 
+ <table border="1" bgcolor="#9cf">
+     <th>Ultimos Usuarios Cadastrados</th>
+     <th>Ultimos Clientes Cadastrados</th>
+     <th>Ultimos Fornecedores Cadastrados</th>
+     <th>Ultimos Funções Cadastradas</th>
+     <th>Ultimos Funcionarios Cadastrados</th>
+     
+     <tr>
+     <td>USUARIO</td>
+     <td>CLIENTES</td>
+     <td>FORNECEDORES</td>
+     <td>FUNÇÕES</td>
+     <td>FUNCIONARIOS</td>
+     </tr>
+
+ </table>
+   
+   
+</fieldset>
+<fieldset>
+ <legend>DADOS FINANCEIRO</legend>
+   <br/> 
+ <table border="1" bgcolor="#9cf">
+     <th>Receita</th>
+     <th>Despesa</th>
+     <th>Faturamentos</th>
+     <th>Total de Vendas</th>
+     <th>Total de Serviços</th>
+     
+     <tr>
+     <td>1</td>
+     <td>2</td>
+     <td>3</td>
+     <td>4</td>
+     <td>5</td>
+     </tr>
+
+ </table>
+</fieldset>
+<fieldset>
+ <legend>DADOS ESTOQUE</legend>
+   <br/> 
+ <table border="1" bgcolor="#9cf">
+     <th>Quantidade de Itens</th>
+     <th>Itens com Baixo Estoque</th>
+     <th>Poriduto com Pouc Saida</th>
+     <th>Valor do Estoque</th>
+     <th>Quantidade de Saida Diaria</th>
+     
+     <tr>
+     <td>1</td>
+     <td>2</td>
+     <td>3</td>
+     <td>4</td>
+     <td>5</td>
+     </tr>
+
+ </table>
+</fieldset>
+<fieldset>
+ <legend>DATAS COMEMORATIVAS</legend>
+   <br/> 
+ <table border="1" bgcolor="#9cf">
+     <th>Aniversariante do Més</th>
+     <th>Aniversariante do Dia</th>
+     <th>Data Parceria do Més</th>
+     <th>Data Parceria do Dia</th>
+     <th>Funcionario do Més</th>
+     
+     <tr>
+     <td>1</td>
+     <td>2</td>
+     <td>3</td>
+     <td>4</td>
+     <td>5</td>
+     </tr>
+
+ </table>
+</fieldset>
